ed7b26aeafc1d923eaf034f9c256b7ad4c715147
Ryan Whalley <ryanwhalley@gmail.com>
2018-11-02 12:11:17 +0000

    Finished all CRUD functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,5 +39,14 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.find(params[:id])
   end
 
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    @portfolio_item.destroy
+    
+    respond_to do |format|
+        format.html { redirect_to portfolios_url, notice: 'Record was successfully deleted.' }
+    end
+  end
   
 end
