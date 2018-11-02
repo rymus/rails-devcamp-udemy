311a0f2f2fd0356a16c31f86129b697b48b890d0
Ryan Whalley <ryanwhalley@gmail.com>
2018-11-02 12:50:09 +0000

    Custom show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
+  
   resources :blogs
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
