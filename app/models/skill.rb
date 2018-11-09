8da8cb369ea4fe1fdbd2cf4e58694cadfb1c66c0
Ryan Whalley <ryanwhalley@gmail.com>
2018-11-09 13:47:55 +0000

    Implemented concern for managing images

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,14 @@
 class Skill < ApplicationRecord
+  include Placeholder
+  
   validates_presence_of :title, :percent_utilized
+  
+  after_initialize :set_defaults
+  #after_initialize runs after the .new action is called
+  #after_create runs after the .create action is called
+  def set_defaults
+    # Calling the placeholder image generator in concerns/placeholder.rb
+    self.badge ||= Placeholder.image_generator(height: '250', width: '250')
+    
+  end
 end
